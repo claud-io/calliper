1ac0ed14c7620255ca08b30c0c15f42441ccc699
claud-io <laura@MacBook-Air-di-Laura.local>
2022-10-09 18:40:24 +0200

    added headless

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,5 +1,11 @@
-export interface item {
+export interface Item {
   date: string | number;
   value: number;
   commentsCount: number;
+}
+
+export interface Comment {
+  description: string;
+  username: string;
+  date: string | number;
 }
